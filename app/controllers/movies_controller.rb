f08e0270e987db3d84b5c1d55c9942d3f2bcd849
Shannon Horcicka <shannon.horcicka1@tafensw.net.au>
2015-04-08 22:54:31 +1000

    2.4 Controller Views Generated
    *Views generated for index, new, edit & show pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,27 @@
 class MoviesController < ApplicationController
+	
 	def index
+		@content_first = 'Movie Content 1';
+		@content_second = 'Movie Content 2';
+	end
+
+	def new
 
 	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
 end
